c2257e1d155e217659ea2892f9303a050316b426
ElayyanAreej <areej.hossein@gmail.com>
2021-08-23 17:53:24 +0300

    get books

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,8 +2,34 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
+import axios from 'axios';
+import BooksCard from './BooksCard';
+
 
 class MyFavoriteBooks extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      books:[],
+      showData:false
+    }
+  }
+  getBooks = async (e) => {
+    e.preventDefault();
+    console.log("hiiii");
+
+    
+    let booksData = await axios.get(`${process.env.REACT_APP_SERVER}/books`);
+    console.log(`${process.env.REACT_APP_SERVER}/books`);
+    console.log(booksData);
+
+    this.setState ({
+      books: booksData.data,
+      showData:true
+    })
+    console.log(this.state.books)
+  }
   render() {
     return(
       <Jumbotron>
@@ -11,6 +37,20 @@ class MyFavoriteBooks extends React.Component {
         <p>
           This is a collection of my favorite books
         </p>
+        
+        {this.state.showData &&
+          this.state.books.map((item, idx) => {
+            return (
+              <BooksCard
+                Key={idx}
+                title={item.title}
+                description={item.description} 
+                status={item.status}
+                email={item.email}/>
+              )})
+                }
+
+        <button onClick={this.getBooks}>view the books</button>
       </Jumbotron>
     )
   }
---
+++ src/BooksCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+class BooksCard extends React.Component {
+  render() {
+    return (
+        <Card style={{ width: "18rem" }}>
+        <Card.Body>
+        <Card.Title>{this.props.title}</Card.Title>
+    <Card.Text>
+    {this.props.description}
+    </Card.Text>
+          <Card.Subtitle className="mb-2 text-muted">
+          Status:{this.props.status}
+          </Card.Subtitle>
+          <Card.Subtitle className="mb-2 text-muted">
+            Email:{this.props.email}
+          </Card.Subtitle>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
+
+export default BooksCard;
