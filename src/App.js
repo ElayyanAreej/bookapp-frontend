15584f15d119eaa38d7c153ef86b0f02d67b26e8
ElayyanAreej <areej.hossein@gmail.com>
2021-08-23 10:14:46 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
-import React from 'react';
-import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
-import Footer from './Footer';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React from "react";
+import Header from "./Header";
+import IsLoadingAndError from "./IsLoadingAndError";
+import { withAuth0 } from "@auth0/auth0-react";
+import Profile from "./Profile";
+import BeastBooks from "./BestBooks";
+import Login from "./Login";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
-
   render() {
-    console.log('app', this.props);
-    return(
+    console.log("app", this.props);
+    const { isAuthenticated  } = this.props.auth0;
+
+    return (
       <>
         <Router>
           {/* <IsLoadingAndError> */}
-            <Header />
-            <Switch>
-              <Route exact path="/">
-                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-               
-              </Route>
-              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-              <Route exact path="/profile">
-              <profile/>      
-              </Route>
-            </Switch>
-            <Footer />
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+              {isAuthenticated ? <BeastBooks /> : <Login />}
+            </Route>
+            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Login />
+          </Switch>
+          <Footer />
           {/* </IsLoadingAndError> */}
         </Router>
+
       </>
     );
   }
 }
 
-export default App;
+export default withAuth0(App);
